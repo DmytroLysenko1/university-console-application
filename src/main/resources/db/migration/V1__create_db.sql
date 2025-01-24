6563bd6f7101ad3d2c1df74c55b7962c918be848
SonnyRose <keeganwunder0@gmail.com>
2025-01-25 00:26:22 +0200

    das

--- src/main/resources/db/migration/V1__create_db.sql
+++ src/main/resources/db/migration/V1__create_db.sql
@@ -1,7 +1,7 @@
 DO
 $$
     BEGIN
-        IF NOT EXISTS (SELECT 1 FROM pg_catalog WHERE datname = 'university') THEN
+        IF NOT EXISTS (SELECT 1 FROM  pg_catalog WHERE datname = 'university') THEN
             CREATE DATABASE university WITH ENCODING = 'UTF8';
         END IF;
     END
